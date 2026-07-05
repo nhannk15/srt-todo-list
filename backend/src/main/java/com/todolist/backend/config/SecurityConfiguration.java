228f13092b00aaf78509c8e1c6cc7f18c6f254f5
William <nhannk15@gmail.com>
2026-07-05 12:02:00 +0700

    feat: added 2 entities MyUser and Task

---
+++ backend/src/main/java/com/todolist/backend/config/SecurityConfiguration.java
@@ -0,0 +1,16 @@
+package com.todolist.backend.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfiguration {
+    
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity security) {
+        return null;
+    }
+}
---
+++ backend/src/main/java/com/todolist/backend/model/entity/MyUser.java
@@ -0,0 +1,39 @@
+package com.todolist.backend.model.entity;
+
+import java.util.List;
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "tbl_users")
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+public class MyUser {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "fullname")
+    private String fullname;
+
+    @Column(name = "googleId")
+    private String googleId;
+
+    @OneToMany(cascade = CascadeType.ALL)
+    private List<Task> tasks;
+
+}
